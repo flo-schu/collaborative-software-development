23358296f6bc819bc9b262d53c2b9153e3013e6d
merkuns <mholdreich@uni-osnabrueck.de>
2025-02-27 16:11:35 +0100

    Fixed pyproject.toml and added two students in main.py

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ def main():
     # Hard-coded relationships
     flo = Student("Flo")
     waldo = Student("Waldo")
+    markus = Student("Markus")
+    holzmichel = Student("Holzmichel")
 
     # Establish relationships
     flo.add_neighbor(waldo)
+    markus.add_acquaintance(flo)
+    markus.add_neighbor(holzmichel)
 
     # Add students to the team
     team.add_student(flo)
+    team.add_student(markus)
+    team.add_student(holzmichel)
 
     # Draw the graph of connections based on neighbors
     team.draw_graph(connection_type='neighbors')  # Change to 'acquaintances' to visualize acquaintances
