88aad175934f1a3f173c371ab9e59a170749a59b
Vincent Hundertmark <vhundertmark@uni-osnabrueck.de>
2025-02-27 16:01:00 +0100

    Habe mich hinzugefuegt o7

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def main():
     # Add students to the team
     team.add_student(flo)
 
+    team.add_student(Student("Vincent"))
+
     # Draw the graph of connections based on neighbors
     team.draw_graph(connection_type='neighbors')  # Change to 'acquaintances' to visualize acquaintances
 
