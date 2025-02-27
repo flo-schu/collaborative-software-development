61d03260c9058d9f7fbb6bde6bb1606b83d126e5
amelieleo <aleonardi@uni-osnabrueck.de>
2025-02-27 16:14:57 +0100

    Add myself as a teammate: Amelie

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def main():
     # Hard-coded relationships
     flo = Student("Flo")
     waldo = Student("Waldo")
+    amelie = Student("Amelie")
 
     # Establish relationships
     flo.add_neighbor(waldo)
+    amelie.add_neighbor(flo)
 
     # Add students to the team
     team.add_student(flo)
+    team.add_student(amelie)
 
     # Draw the graph of connections based on neighbors
     team.draw_graph(connection_type='neighbors')  # Change to 'acquaintances' to visualize acquaintances
